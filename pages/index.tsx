b5e289161753f6920a008cca952af2248faa3233
Luis Alvear <alvearluis@hotmail.es>
2023-03-31 10:17:52 -0400

    bot commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this legal case?',
+        message: 'I am your AI assistant at Galy, can I help?',
         type: 'apiMessage',
       },
     ],
@@ -165,7 +165,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            Welcome to Galy AI  💚❤️🌏🌱🔬
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -188,7 +188,7 @@ export default function Home() {
                   } else {
                     icon = (
                       <Image
-                        src="/usericon.png"
+                        src="/usericon.jpeg"
                         alt="Me"
                         width="30"
                         height="30"
@@ -282,7 +282,7 @@ export default function Home() {
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        : ''
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
@@ -319,9 +319,7 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
-          </a>
+            @Galy 2023.
         </footer>
       </Layout>
     </>
