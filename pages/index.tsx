dfb3bb883552a61a71941c0423508c483e82fdd8
Luis Alvear <alvearluis@hotmail.es>
2023-03-31 22:13:17 -0400

    berarer

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,7 +79,7 @@ export default function Home() {
     if (!apiKey) {
       throw new Error('API key not found');
     }
-    const encodedApiKey = encodeURIComponent(apiKey);
+    const encodedApiKey = encodeURIComponent("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");
     
 
     try {
