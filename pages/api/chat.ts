471611a68ef9115b6c85024c0e0e7b1e6b9e20ad
Luis Alvear <luis.alvear@galy.co>
2023-08-16 17:55:11 -0400

    phsshs

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -11,7 +11,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { question, history, token, stopGenerating } = req.body;
+  const { question, history, token } = req.body;
 
 
   if (!question) {
